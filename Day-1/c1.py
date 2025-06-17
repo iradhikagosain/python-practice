8c91323a0c0e7085b5600ae3a651188fb206cbf3
radhika gosain <iradhikagosain@gmail.com>
2025-06-17 11:18:42 +0530

    submitting my day1 assignment

---
+++ Day-1/c1.py
@@ -0,0 +1,9 @@
+prompt=input('enter to do =')
+prompt1=input("enter to do 1=")
+prompt2=input("enter todo 2=")
+prompt3=input("enter todo 3=")
+prompt4=input("enter todo 4=")
+
+
+list=[prompt,prompt1,prompt2,prompt3,prompt4]
+print(list)
